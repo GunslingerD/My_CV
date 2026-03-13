bb09ec045f399bd63422003243ac5477844b4d94
GunslingerD <136006787+GunslingerD@users.noreply.github.com>
2026-03-13 11:23:44 -0600

    Added weeksleft.py to the repository

---
+++ Python_Projects/weeksleft.py
@@ -0,0 +1,8 @@
+def life_in_weeks(age):
+    weeks_used = age * 52
+    lifespan = 90*52
+    weeks_left = lifespan - weeks_used
+    print(f"You have {weeks_left} weeks left")
+          
+
+life_in_weeks(56)
